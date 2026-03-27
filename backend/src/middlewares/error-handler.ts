7c1c586a9f03c212430232badaaaf5e01a44e52c
Pavel Savelev <9173912682pavel@gmail.com>
2026-03-27 10:52:42 +0300

    fix post test

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,9 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
 import { isCelebrateError } from 'celebrate';
+import { Request, Response, NextFunction } from 'express';
 import BaseError from '../errors/default-error';
 
 function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
-  // Celebrate / Joi ошибка
   if (isCelebrateError(err)) {
     const bodyError = err.details.get('body');
     return res.status(400).json({
@@ -11,12 +10,10 @@ function errorHandler(err: any, _req: Request, res: Response, _next: NextFunctio
     });
   }
 
-  // твои кастомные ошибки
   if (err instanceof BaseError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
 
-  // все остальные ошибки
   console.error(err);
   return res.status(500).json({ message: 'Ошибка сервера' });
 }
