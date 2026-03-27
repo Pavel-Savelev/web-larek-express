56fd7c3e92b2b68dc390e6c3356cfe0bc8783d88
Pavel Savelev <9173912682pavel@gmail.com>
2026-03-27 10:38:12 +0300

    fix post test

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,12 +1,22 @@
-// middlewares/error-handler.ts
 import { Request, Response, NextFunction } from 'express';
+import { isCelebrateError } from 'celebrate';
 import BaseError from '../errors/default-error';
 
 function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
+  // Celebrate / Joi ошибка
+  if (isCelebrateError(err)) {
+    const bodyError = err.details.get('body');
+    return res.status(400).json({
+      message: bodyError?.details[0].message || 'Ошибка валидации',
+    });
+  }
+
+  // твои кастомные ошибки
   if (err instanceof BaseError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
 
+  // все остальные ошибки
   console.error(err);
   return res.status(500).json({ message: 'Ошибка сервера' });
 }
